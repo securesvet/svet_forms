ed79374309caa80a423c717f1e61fe294545bf2a
Sviatoslav <mursvyats@gmail.com>
2024-01-03 17:03:17 +0500

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import React from "react";
+import {Button} from '@/components/ui/button'
+
 export default function Home() {
   return (
     <main className="grid place-items-center">
       <h1>Hello world!</h1>
+        <Button className="bg-white text-red-500">Press me</Button>
     </main>
   )
 }
